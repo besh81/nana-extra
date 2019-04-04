179eb0d270737040d7eaed0127634d321f490eaf
James Bremner <james@ravenspoint.com>
2019-04-04 08:56:48 -0400

    Allow changes to scrolled panel size
    Also handle scrolled panel smaller than visible panel.

--- panel_scrolled/panel_scrolled.h
+++ panel_scrolled/panel_scrolled.h
@@ -3,7 +3,9 @@
 #include <nana/gui/widgets/label.hpp>
 #include <nana/gui/widgets/scroll.hpp>
 
-namespace nana {
+
+namespace nana
+{
 
 /** Panel specialization to display small visible scrollable part of the panel
 
@@ -52,25 +54,24 @@ public:
     {
         // create base panel over which scrollable window moves
         create( myVisible, true );
-        move( scrolled_panel );
+        panel<false>::move( scrolled_panel );
 
         // place scrolling window and scrolls in position on parent
         myVisibleAndScrolls.move( visible_panel );
 
         // Set scroll ammounts to the full extent of the scrolled area
-        myScrollHoriz.amount( scrolled_panel.width  - visible_panel.width );
-        myScrollVert.amount(  scrolled_panel.height - visible_panel.height );
+        scroll_amount();
 
         // register scroll event handlers
         myScrollHoriz.events().value_changed([&]
         {
-            move(
+            panel<false>::move(
                 -myScrollHoriz.value(),
                 -myScrollVert.value() );
         });
         myScrollVert.events().value_changed([&]
         {
-            move(
+            panel<false>::move(
                 -myScrollHoriz.value(),
                 -myScrollVert.value() );
         });
@@ -83,13 +84,34 @@ public:
         myPlace.collocate();
 
     }
+    /// Change size of scrolled panel
+    void size( const nana::size& s )
+    {
+        panel<false>::size( s );
+        scroll_amount();
+    }
+    void move( const rectangle& r)
+    {
+        throw std::runtime_error("move disabled for scrolled_panel, use size instead");
+    }
 private:
     nana::panel<false> myVisibleAndScrolls;   /// Window showing scrolling window and scrolls
     nana::panel<false> myVisible;           ///< Part of scrolled panel that is visible
     nana::scroll< false > myScrollHoriz;
     nana::scroll< true  > myScrollVert;
     nana::place myPlace;
+
+    /// Adjust scrolling amounts
+    void scroll_amount()
+    {
+        int x = panel<false>::size().width  - myVisible.size().width;
+        if( x < 0 )
+            x = 0;
+        int y = panel<false>::size().height - myVisible.size().height;
+        if( y < 0 )
+            y = 0;
+        myScrollHoriz.amount( x );
+        myScrollVert.amount( y );
+    }
 };
 }
-
-
